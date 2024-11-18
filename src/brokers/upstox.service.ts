0e936f7558112728f0f2660d85c672713e447151
Yadvendra Sharma <yadvendras20@gmail.com>
2024-11-18 20:31:37 +0530

    upstox webhook

--- src/brokers/upstox.service.ts
+++ src/brokers/upstox.service.ts
@@ -171,7 +171,7 @@ export class UpstoxBroker {
               transaction_type: side,
               disclosed_quantity: 0,
               trigger_price: triggerPrice,
-              is_amo: false,
+              is_amo: true,
             },
           };
           //check if the quantity exceeds the freeze quqntity for that perticular index? if it does, then slice the order
